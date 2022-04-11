deccaed1205698850842c2058d4ce6c98e6d7a79
Dark Dragoni <>
2022-04-11 01:19:45 +0000

    Task2

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-# % foramatting
-print('%.3f' % (11 /  3))
-name = 'Mary'; age = 15
-print('Hi %s you are %d' % (name, age))
-
-# .format()
-print('{} {} {}'.format('Me', 'and', 'You'))
-print('{who} {} {}'.format('Me', 'and', who='You'))
-print('{who} {} {}'.format('Me', 'and', who='You'))
-print('{2} {1} {0}'.format('Me', 'and', 'You'))
-
-# f'{var]'
-a = 5
-print(f'%.{a}f' % (11 /  3))
-name = 'Jonny Cage'
-print(f'This is the end of you, {name}')
+print('Film {}, author {} came out {}'.format('Fight Club', 'David Filcher', '1999'))
+print('Film {0} {2}, {1}'.format('Fight Club', 'David Filcher', '1999'))
