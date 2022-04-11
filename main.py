09dfadcf9ed968a0d32598891254789f1016e574
Dark Dragoni <>
2022-04-11 01:33:30 +0000

    task3

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-nums = [n * 2 for n in range(1, 21)]
-print(nums)
-print(nums[2:7])
-
-str1 = 'Python is not just a mere snake'
-print(str1[10:18])
-
-print(str1[::-1])
-print(str1[7::])
-
-new_str = str1[:]
-print(new_str)
-
-my_list = ['Java', 'Python', 'Kotlin']
-print(my_list[:2])
-print(my_list[:99999999])
+print('{}(dir {}) came out in {}'.format('Fight Club', 'David Filcher', '1999'))
+print('Film {0} {2}, {1}'.format('Fight Club', 'David Filcher', '1999'))
