0f75816b9364f66471beaca20ec8f23ffd4a5d1a
Dark Dragoni <>
2022-04-11 02:02:30 +0000

    task2

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-print('{}(dir {}) came out in {}'.format('Fight Club', 'David Filcher', '1999'))
-print('Film {0} {2}, {1}'.format('Fight Club', 'David Filcher', '1999'))
+a= 2.71828
+b= 3
+print('%.5s' % a)
