92b21adfdca2c196f5c9c9ccd2d1d1f6f9e00db6
Dark Dragoni <>
2022-04-11 01:24:54 +0000

    Слайсинг

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-nums = [n for n in range(20)]
+nums = [n * 2 for n in range(1, 21)]
 print(nums)
-print(nums[2:5])
-name = 'Python is not a snake'
-b = name[7:2:-1]
-print(b)
+print(nums[2:7])
+
+str1 = 'Python is not just a mere snake'
+print(str1[10:18])
+
+print(str1[::-1])
+print(str1[7::])
+
+new_str = str1[:]
+print(new_str)
+
+my_list = ['Java', 'Python', 'Kotlin']
+print(my_list[:2])
+print(my_list[:99999999])
