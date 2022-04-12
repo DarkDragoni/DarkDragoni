71a471adeba3a77173cad74b28ad29c306729343
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-12 00:38:11 +0000

    #map #filter

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-# task 1 slice
+# map
 
-def isPalindrome(w):
-    return w == w[::-1]
- 
-w = "palindrom"
-ans = isPalindrome(w)
- 
-if ans:
-    print("Yes")
-else:
-    print("No")
+  
+
+nums = [n for n in range(10)]
+# # double_n = [n * 2 for n in nums]
+# print(nums)
+# # print(double_n)
+
+# def doubler(x):
+#   return x * 2
+
+# double_numbers = list(map(doubler, nums))
+# print(double_numbers)
+
+# a, b = map(int, (input(), input()))
+# print(a, b)
+
+
+double_numbers = list(map(lambda x: x * 2, nums))
+
+print(double_numbers)
+
+
+x_list = [1, 2, 4]
+y_list = [4, 6, 8]
+z_list = [9, 10, 10]
+
+res = list(map(lambda x, y, z: x + y + z, x_list, y_list, z_list))
+print(res)
+
+#filter
+
+odd_numbers = list(filter(lambda x: x % 2, nums))
+print(odd_numbers)
