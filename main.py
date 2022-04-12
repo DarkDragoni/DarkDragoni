a213177daf98c9775a8914f1c37c689545c8f6b2
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-12 02:44:55 +0000

    Progekt DOMINO

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-stock = [[i, j] for i in range(7) for j in range(i, 7)]
+from random import randint, shuffle
+
+stock = [[i,j] for i in range(7) for j in range(i,7)]
+doubles = stock [:-4:-2]
+on_hands = 7
+
+
+while not any([double in stock[:on_hands *2] for double in doubles]):
+  shuffle(stock)
+
+
+for double in doubles:
+  if double in stock[:on_hands * 2]:
+    snake = stock.pop(stock.index(double))
+    break
+computer, player = stock[:on_hands - 1], stock[on_hands - 1:on_hands * 2 - 1]
+if randint (1, 2) == 2:
+  player, computer = computer, player
+
+stock = stock[on_hands * 2 - 1:]
+
+next_one = 'computer' if len(computer) > len(player) else 'player'
+print('stock pieces: {}\n\
+    computer pieces: {}\n\
+      player pieces: {}\n\
+      domino pieces: [{}]\n\
+            status; {}'.\
+            format(stock, computer, player, snake, next_one))
