5c5a99ef19a648af76893483b4181c8a82770f0b
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-12 11:13:33 +0000

    2 task8

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-nums = [0, 1, 1, 2, 3, 5, 8]
-copy = (nums[:])
-print(copy)
+my_list = [3, 9, 27, 81, 243, 729, 2187]
+print(my_list[-1])
+print(my_list[:])
+print(my_list[2:-1:-3])
+print(my_list[2:0:-3])
+print(my_list[7])
