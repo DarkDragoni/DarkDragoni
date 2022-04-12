b343125f4bea0af5222b65cb2479d56ddc6bac41
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-12 23:40:32 +0000

    3 task1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-my_list = [3, 9, 27, 81, 243, 729, 2187]
-print(my_list[-1])
-print(my_list[:])
-print(my_list[2:-1:-3])
-print(my_list[2:0:-3])
-print(my_list[7])
+n = 10 
+k = 3
+a1 = (n - k) * (n+k)
+1 <= n <= 10^3
+0 <= k <= n
+print(a1)
