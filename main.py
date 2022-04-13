7dd3c583c98b91f43c7029f051e41f402130c403
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-13 23:22:21 +0000

    Exeption

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-from string import ascii_letters
-print(ascii_letters)
+# Handling Exceptions
+
+# list
+# https://docs.python.org/3/library/exceptions.html
+
+a = 5
+b = 'five'
+
+
+try:
+  result = a / b
+except ZeroDivisionError as err:
+  print('0 Error', err)
+except TypeError as error:
+  print('T Error', error.__class__.__name__, e)
+else:
+  print('The result is ', result)
+finally:
+  print("Thank you for using the service")
 
 
