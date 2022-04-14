f78a539b19509533f348200fae498caea0c8eac3
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-14 23:52:52 +0000

    4 Task2

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-#always young
-# 4 task 01
-from string import ascii_lowercase
-from random import choice
-from random import randint
-lst = []
-new_list = []
-for _ in range(randint(1, len(ascii_lowercase))):
-  lst.append(choice(ascii_lowercase))
-for _ in range(randint(1,len('aiueo'))):
-  new_list.append(choice('aiueo'))
-print(new_list)
+# 4 task 2
+nums = [n for n in range(5)]
+even = list(map(lambda x: x * 2, nums))
+
+odd = [1, 3, 5, 7, 9]
+
+length = len(even)
+
+my_sum = []
+i = 0
+while i < length:
+    my_sum.append(even[i] + odd[i])
+    i = i + 1
+
+remainders = list(map(lambda x: x % 3, my_sum))
+
+onzero_remainders = list(filter(lambda x: x % 3, remainders))
+
+print(my_sum)
+print(remainders)
+print(onzero_remainders)
