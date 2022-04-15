a038a7b105d489020be7892b054adb0f6ff95db9
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-15 02:57:21 +0000

    Prime nums

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
-nums = [n for n in range(11)]
-inputs = nums
-a = list(map(lambda x: x * 10, inputs))
-b = inputs * 10
-c = [10 * x for x in inputs]
-if a == c:
-  print('a and c perform the same task')
-if a == b:
-  print('a and b perform the same task')
-if b == c:
-  print('b and c perform the same task')
+from time import perf_counter
+
+def get_primes(n):
+  primes = [2, 3]
+  for i in range(5, n + 1):
+    if i % 6 in (1, 5):
+      prime = True
+      for j in range(2, int(i ** 0.5) + 1):
+        if not i % j:
+          prime = False
+          break
+      if prime:
+        primes.append(i)
+  return primes
+
+
+n = 100_000
+start = perf_counter()
+primes = get_primes(n)
+print(perf_counter() - start)
+print(primes)
+
+
+def get_prime_divs(n):
+  res = []
+  for i in range(2, int(n ** 0.5) + 1):
+    while not n % i:
+      if not i in res:
+        res.append(i)
+      n //= i
+    if n < i:
+      break
+  if n > 1:
+     res.append(n)
+  return res
+
+n = 999_999_999
+start = perf_counter()
+print(get_prime_divs(n))
+print(perf_counter() - start)
