2ad5d7b68966154b99395e18759855663dee550b
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-15 01:36:30 +0000

    4 Task3

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-lst = ['Денис', 'Дима0', 'Ваня', 'Дима1', 'Дима2', 'Лёша', 'Вова', 'Вадим', 'Богда', 'Юра', 'Антон', 'Артём', 'Костя']
-lst.sort()
-i = 1
-for i, name in enumerate(lst):
-  print("student",i + 1,"->", name)
+# 4 task 03
+from random import randint
+
+
+names = ['Денис', 'Дима1', 'Ваня', 'Дима2', 'Дима3', 'Лёша', 'Вова', 'Вадим', 'Богда', 'Юра', 'Антон', 'Артём', 'Костя']
+names.sort()
+
+math = [randint(25, 50) for _ in range(len(names))]
+eng = [randint(25,50) for _ in range(len(names))]
+phys = [randint(25,50) for _ in range(len(names))]
+
+res = list(map(lambda x, y, z: x + y + z, math, eng, phys))
+
+enroll = []
+
+for names, point in zip(names, res):
+  if point > 100:
+    enroll.append([names, point])
+
+print(enroll)
