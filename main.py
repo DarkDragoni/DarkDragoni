28f81bbbb917296a282bd39504bcf0d24f11357c
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-15 00:53:09 +0000

    5 Task1

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-# 4 task 2
-nums = [n for n in range(5)]
-even = list(map(lambda x: x * 2, nums))
-
-odd = [1, 3, 5, 7, 9]
-
-length = len(even)
-
-my_sum = []
-i = 0
-while i < length:
-    my_sum.append(even[i] + odd[i])
-    i = i + 1
-
-remainders = list(map(lambda x: x % 3, my_sum))
-
-onzero_remainders = list(filter(lambda x: x % 3, remainders))
-
-print(my_sum)
-print(remainders)
-print(onzero_remainders)
+lst = ['Денис', 'Дима0', 'Ваня', 'Дима1', 'Дима2', 'Лёша', 'Вова', 'Вадим', 'Богда', 'Юра', 'Антон', 'Артём', 'Костя']
+lst.sort()
+i = 1
+for i, name in enumerate(lst):
+  print("student",i + 1,"->", name)
