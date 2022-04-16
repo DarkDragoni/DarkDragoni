0b2dbc02232c249c8b515e44d90f323de5512220
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-16 01:42:43 +0000

    IMPORT random

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
-my_list = [i for i in range(-10,11)]
-daily_temp_c = [20, 19, 15, 25, 27, 30, 31, 29, 26, 21,
-                19, 25, 27.5, 28, 26, 29.5, 31, 27.5, 26, 29,
-                18, 17.5, 17, 16.5, 19, 20, 25, 26.5, 27, 28,
-                20.5, 19, 25, 27.5, 28, 26, 15, 25, 27, 28]
-
-def find_positive(my_list):
-   return my_list > 0
-
-positive = list(filter(find_positive, my_list))
-  
-
-print(positive)
-        
-def celsius_to_fahrenheit(daily_temp_c):
-   return (daily_temp_c * 9 / 5) + 32
-
-fahrenheit = list(map(celsius_to_fahrenheit, daily_temp_c))
+import random
+from functools import reduce
 
 
-def temp_above_80 (fahreinheit):
-  return fahreinheit < 80
+letters  = [chr(n) for n in range(65, 80)]
+freqs = [n + 1 for n in range(len(letters))][::-1]
+print(letters)
+print(freqs)
 
-days = list(filter(temp_above_80, fahrenheit))
 
+print(random.choices(letters, freqs))
 
+password = [random.choices(letters, freqs) for _ in range(6)]
+print(password)
 
-for i, name in enumerate(days, start=1):
-  print(i, name)
+print(''.join(reduce(lambda a, b: a + b, password)))
+
