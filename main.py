a3053c8a1e7256513a50e579a0c752bf156210b2
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-16 01:49:13 +0000

    4 task8

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-print('The outputs for A is {}. And for B is {}'.format('[11, 13, 15, 17]', '[1, 2, 3, 4, 10, 11, 12, 13]'))
-list_1 = [i for i in range(0,10)]
-list_2 = [i for i in range(10, 20)]
+my_list = [i for i in range(-10,11)]
 
-def my_product():
- res = list(map(lambda x, y: x * y ,list_1, list_2, ))
- return res
-print(my_product())
+def find_positive(my_list):
+   return my_list > 0
+
+positive = list(filter(find_positive, my_list))
+
+print(positive)
