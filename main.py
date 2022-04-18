f4832276d00bccf934f5e0b2f5e5a290b24de59d
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-18 23:47:53 +0000

    5 task5

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-stock = [[2, 1], [2, 3]]
+letters = ['A', 'B', 'C']
 
-weights = {
-  0: 7,
-  1: 8
-}
+my_iterator = iter(letters)
 
-max_value = max(weights.values())
-print(max_value)
-
-rev_weights = {v: k for k, v in weights.items()}
-
-max_key = stock[rev_weights[max_value]]
-print(max_key)
+print(next(my_iterator))
+# 'A'
+print(next(my_iterator))
+# 'B'
+print(next(my_iterator))
+# 'C'
+print(next(my_iterator))
