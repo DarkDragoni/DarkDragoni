57e182267690ee63e7ae6965d579cb15adb98dc3
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-19 00:07:36 +0000

    6 task1

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
-letters = ['A', 'B', 'C']
+from time import perf_counter
+from time import perf_counter
 
-my_iterator = iter(letters)
+def get_primes(n):
+  primes = [2, 3]
+  for i in range(5, n + 1):
+    if i % 6 in (1, 5):
+      prime = True
+      for j in range(2, int(i ** 0.5) + 1):
+        if not i % j:
+          prime = False
+          break
+      if prime:
+        primes.append(i)
+  return primes
 
-print(next(my_iterator))
-# 'A'
-print(next(my_iterator))
-# 'B'
-print(next(my_iterator))
-# 'C'
-print(next(my_iterator))
+
+n = 10
+start = perf_counter()
+primes = get_primes(n)
+print(primes)
+print(perf_counter() - start)
+
+
+def get_prime_divs(n):
+  res = []
+  for i in range(2, int(n ** 0.5) + 1):
+    while not n % i:
+      if not i in res:
+        res.append(i)
+      n //= i
+    if n < i:
+      break
+  if n > 1:
+     res.append(n)
+  return res
+
+n = 10
+start = perf_counter()
+print(get_prime_divs(n))
+print(perf_counter() - start)
