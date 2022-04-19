7d0e531e734929fef623519b2388d4386ddfa693
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-19 01:31:47 +0000

    Написание в новые файлы непосредственно с моего

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
-Блок except...
+#READ
 
-a - будет выполнен, если в блоке try не было исключений
+with open('passwords.txt', 'r', encoding='utf-8') as file:
+  print(file.read(4)) #  read 4 bytes
+  print(file.read())  #  read all/rest of bytes in fileif read before like above
 
-# b - будет выполнен только в том случае, если произошло указанное исключение
+with open('passwords.txt', 'r', encoding='utf-8') as file:
+  print(file.readline(5)) #  read 5 bytes
+  print(file.readline())  #  read all/rest of bytes in line if read before like above
 
-c - будет выполняться всегда в конце
+with open('passwords.txt', 'r', encoding='utf-8') as file:
+  print(file.readlines()) #  returns list
+  
+with open('passwords.txt', 'r', encoding='utf-8') as file:
+  for line in file:
+    print(line) #  returns line + \n
+    print(line.strip()) #  returns line stripped from \s\n\t etc.
 
-d - не будет выполняться, если исключение в блоке try не соответствует указанному исключению
+
+#WRITE
+with open('checked.txt', 'w', encoding='utf-8') as file:
+  file.write('new entry - check')
+  file.write('NEW new entry - check')  # re-writes the line above
+
+with open('checked.txt', 'a', encoding='utf-8') as file:
+  file.write('new entry - check')
+  file.write('NEW new entry - check')  # appends the line to existing contents
+
+checked = ['this', 'that', 'that over there']
+with open('checked.txt', 'w', encoding='utf-8') as file:
+  for check in checked:
+    file.write(check + '\n')  # writes all in the list each on new line
+
+checked = ['this\n', 'that\n', 'that over there\n']
+with open('checked.txt', 'w', encoding='utf-8') as file:
+    file.writelines(checked)  # writes all in the list each on new line
