622e470674f1010312ff28813300ed381e198dad
Dark Dragoni <103391828+DarkDragoni@users.noreply.github.com>
2022-04-19 00:14:34 +0000

    7 task1

--- main.py
+++ main.py
@@ -1,19 +1,2 @@
-from time import perf_counter
-
-def get_prime_divs(n):
-  res = []
-  for i in range(2, int(n ** 0.5) + 1):
-    while not n % i:
-      if not i in res:
-        res.append(i)
-      n //= i
-    if n < i:
-      break
-  if n > 1:
-     res.append(n)
-  return res
-
-n = 1_000_000_000
-start = perf_counter()
-print(get_prime_divs(n))
-print(perf_counter() - start)
+a = 5
+c = a + b
